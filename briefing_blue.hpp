dc2f10162c52bd6660bcb34b1b61f22ae538e854
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:44:54 +0200

    tb_rhs_op_zorro.kunduz mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,17 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base at Bravo unlocked upon capture of Charlie<br />
+<RP> Team Rally Point at Charlie<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Have infantry ready to defend Delta before the enemy occupy it<br />
+<*> Attack Echo to slow down the enemy advance<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>Controlling the center is key to this mission</yellow>
---
+++ briefing_red.hpp
@@ -0,0 +1,17 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base at Golf unlocked upon capture of Foxtrot<br />
+<RP> Team Rally Point at Foxtrot<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> Have infantry ready to defend Echo before the enemy occupy it<br />
+<*> Attack Delta to slow down the enemy advance<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>Controlling the center is key to this mission</yellow>
